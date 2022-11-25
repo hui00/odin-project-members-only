2a4545e679178f4011d525708736bdff17efa741
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-25 04:12:18 +0100

    production rdy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "static_pages/landing_page"
+  get "static_pages/dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root "static_pages#landing_page"
 
   # Defines the root path route ("/")
   # root "articles#index"
